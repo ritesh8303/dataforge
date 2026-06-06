c8c81719a6e1a2a109ef03076ccf1362b94d6b02
Ritesh Jadhav <riteshjadhav359@gmail.com>
2026-06-06 22:54:44 +0200

    chore: re-enable Apify EventBridge schedule

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,10 +129,9 @@ module "apify_ingestor" {
   env_vars = {
     BRONZE_BUCKET            = module.s3_bronze.bucket_id
     SSM_APIFY_PARAMETER_NAME = "/dataforge/dev/apify_credentials"
-    DISABLE_APIFY            = "true"
   }
   bronze_bucket_arn = module.s3_bronze.arn
-  enable_schedule   = false
+  enable_schedule   = true
   alert_email       = var.alert_email
 }
 
